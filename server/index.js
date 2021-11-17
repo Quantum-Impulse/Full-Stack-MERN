07303113b65a96f749df0d8fd6e5ecd924505ad5
Enrique Rivera <erivera7240@gmail.com>
2021-11-16 23:18:20 -0600

    adding some server code

--- server/index.js
+++ server/index.js
@@ -3,3 +3,11 @@ import bodyParser from 'body-parser';
 import  Mongoose from 'mongoose';
 import cors from 'cors';
 
+const app = express();
+
+//
+app.use(bodyParser.json( { limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded( { limit: "30mb", extended: true}));
+app.use(cors());
+
+// MangoDB Stuff
