45eaa91299963786c8901bf3f11ed0d3e188f236
Enrique Rivera <erivera7240@gmail.com>
2021-11-17 00:13:02 -0600

    learnt whare the promises and async are

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import  Mongoose from 'mongoose';
+import mongoose from 'mongoose';
 import cors from 'cors';
 
 const app = express();
@@ -10,4 +10,15 @@ app.use(bodyParser.json( { limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded( { limit: "30mb", extended: true}));
 app.use(cors());
 
-// MangoDB Stuff
+const CONNECTION_URL = 'mongodb+srv://erivera7240:123@cluster0.4scrv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true} )
+
+app.listen(PORT, () => {
+    console.log(`now listening on ${PORT}`)
+})
+
+// MangoDB atlas, database  https://www.mongodb.com/cloud/atlas
+
+
