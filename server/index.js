e8795be3f27e70385542d0e688eea3f34940224d
Enrique <erivera7240@gmail.com>
2021-11-20 16:26:14 -0600

    usin the old code now

--- server/index.js
+++ server/index.js
@@ -13,28 +13,30 @@ app.use(cors());
 const CONNECTION_URL = 'mongodb+srv://erivera7240:123@cluster0.4scrv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
-// mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true} )
-//     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT} `)))
-//     .catch( (error) => console.log( error.message) )
+// async function initMongo() {
+//     try {
+//         await mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+//         return [mongoose, null]
+//     } catch (e) {
+//         return [null, e]
+//     }
+// }
 
-async function initMongo() {
-    try {
-        await mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
-        return [mongoose, null]
-    } catch (e) {
-        return [null, e]
-    }
-}
+// async function initServer() {
+//     let [data, error] = await initMongo()
+//     if (data) {
+//         app.listen(PORT)
+//         console.log(`server running`)
+//     } else if (error) {
+//         console.error(`omg there was an error`)
+//     }
+// }
 
-async function initServer() {
-    let [data, error] = await initMongo()
-    if (data) {
-        app.listen(PORT)
-        console.log(`server running`)
-    } else if (error) {
-        console.error(`omg there was an error`)
-    }
-}
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true} )
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT} `)))
+    .catch( (error) => console.log( error.message) )
+
+
 
 initServer()
 
