11de2a5525032045907390a8a1ca5e35c3bfd2cd
Enrique Rivera <erivera7240@gmail.com>
2021-11-28 13:18:01 -0600

    making changes to the form adding some text fields

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
-
+import FileBase from 'react-file-base64';
 import useStyles from './styles';
 
 const Form = () => {
@@ -15,8 +15,12 @@ const Form = () => {
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={classes.name} onSubmit={handleSubmit} >
             <Typography variant="h6">Creating a Memory</Typography>
-            <TextField name="creator"variant="outlined" label="Creator" fullWidthvalue={postData.creator}onChange={(e) => setPostData( { ... postData, creator: e.target.value })}
-             />
+            <TextField name="creator"variant="outlined" label="Creator" fullWidthvalue={postData.creator} onChange={(e) => setPostData( { ... postData, creator: e.target.value })} />
+            <TextField name="title"variant="outlined" label="Title" fullWidthvalue={postData.title} onChange={(e) => setPostData( { ... postData, title: e.target.value })} />
+            <TextField name="message"variant="outlined" label="Message" fullWidthvalue={postData.message} onChange={(e) => setPostData( { ... postData, message: e.target.value })} />
+            <TextField name="tags"variant="outlined" label="Tags" fullWidthvalue={postData.tags} onChange={(e) => setPostData( { ... postData, tags: e.target.value })} />
+            <div className={classes.fileInput}> <FileBase type="file" multiple={false} onDone={({base64}) => setPostData({ ... postData, selectedFile: base64})} /> </div>
+            
             </form>
         </Paper>
     )
