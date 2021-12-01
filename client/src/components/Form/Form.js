2f35ae712cdfb4e58ee255b4e5a1dff809848e01
Enrique Rivera <erivera7240@gmail.com>
2021-12-01 10:57:44 -0600

    stuff for it update doesn't work atm

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,17 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
-import useStyles from './styles';
-import { useDispatch} from 'react-redux';
+import { useDispatch, useSelector} from 'react-redux';
 
+import useStyles from './styles';
 import { createPost, updatePost } from '../../actions/posts';
 
 //Get the current ID we are on
 
 const Form = ({ currentId, setCurrentId}) => {
     const [postData, setPostData] = useState( {creator: '', title: '', message: '', tags: '', selectedFile: ''});
+    const post = useSelector( (state) => currentId ? state.posts.find((p) => p._id === currentId) : null );
     const classes = useStyles();
     const dispatch = useDispatch();
+
+    useEffect( () => {
+        if(post) { setPostData(post); };
+    }, [post])
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -21,7 +26,6 @@ const Form = ({ currentId, setCurrentId}) => {
         }else{
             dispatch(createPost(postData));    
         }
-        
     }
 
     const clear = () => {
--- server/controllers/post.js
+++ server/controllers/post.js
@@ -34,10 +34,9 @@ export const updatePost = async (req, res) => {
     const { id: _id }  = req.params;
     const post = req.body;
 
-    if( mongoose.Types.ObjectId.isValid(_id)) 
-        return res.status(404).send('No post with that ID');
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that ID');
 
     const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true});
 
-    res.json(updatePost);
+    res.json(updatedPost);
 }
