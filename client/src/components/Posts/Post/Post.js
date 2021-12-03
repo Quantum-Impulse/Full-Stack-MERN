60011dd781abd2b3199e4136a859c05c379e5b72
Enrique Rivera <erivera7240@gmail.com>
2021-12-03 15:53:14 -0600

    liking now works just need to automatically refesh

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -5,7 +5,7 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 
-import { deletePost } from '../../../actions/posts';
+import { deletePost, likePost } from '../../../actions/posts';
 import { useDispatch } from 'react-redux';
 import useStyles from './styles';
 
@@ -42,7 +42,7 @@ const Post = ( { post, setCurrentId }) => {
             </CardContent>
             
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={ () => {} }>
+                <Button size="small" color="primary" onClick={ () => dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon fontSize="small"/>
                     Like
                     {post.likeCount}
