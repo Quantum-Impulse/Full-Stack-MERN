7a2e0e27d1bd2b2ea50010eb57e5336bceb05aff
Enrique Rivera <erivera7240@gmail.com>
2021-12-03 15:16:43 -0600

    added backend logic, now working on frontend

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getPosts, createPost, updatePost } from '../controllers/post.js'
+import { getPosts, createPost, updatePost, deletePost } from '../controllers/post.js'
 
 const router = express.Router();
 
@@ -8,6 +8,7 @@ const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
-router.patch('/:id', updatePost )
+router.patch('/:id', updatePost );
+router.delete('/:id', deletePost);
 
 export default router;
