0b2d19cef0fde0e23c11fd9c7deb3d4218de686d
Enrique Rivera <erivera7240@gmail.com>
2021-12-07 00:16:03 -0600

    removed the mongo url and used dotenv instead

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json( { limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded( { limit: "30mb", extended: true}));
@@ -14,7 +16,6 @@ app.use(cors());
 app.use('/posts', postRoutes)
 
 // Adding the link like this is not the best since it's not secure
-const CONNECTION_URL = 'mongodb+srv://erivera7240:123@cluster0.4scrv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' ;
 const PORT = process.env.PORT || 5000;
 
 // async function initMongo() {
@@ -36,7 +37,8 @@ const PORT = process.env.PORT || 5000;
 //     }
 // }
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+// securely using the connection url with dotenv
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT} `)))
     .catch( (error) => console.log( error.message) )
 
